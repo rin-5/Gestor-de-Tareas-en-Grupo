994b90ae22027e0139c916c60aaa47026a12278d
Jonán Vega Rodríguez <nekogamer9hey@gmail.com>
2026-03-04 15:54:18 +0000

    Estructura inicial: Clase Tarea

---
+++ src/gestortareasengrupo/Tarea.java
@@ -0,0 +1,56 @@
+package gestortareasengrupo;
+
+/**
+ * Clase que contiene información sobre una tarea.
+ * @author Jonan Vega
+ * @version 0.1
+ * @since 2026
+ */
+public class Tarea {
+    private String descripcion;
+    private boolean completada;
+
+    /**
+     * Constructor de la clase Tarea, que incluye una descripción de dicha tarea y su estado.
+     * @param descripcion Cadena de texto que contiene la información de una tarea.
+     * @param completada  Estado de la tarea, que devuelve falso si no está completa y verdadero si ya ha sido marcada como completada.
+     */
+    public Tarea(String descripcion, boolean completada) {
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
+    /**
+     * Método que devuelve la descripción de una tarea.
+     * @return Cadena de texto con la descripcion de una tarea.
+     */
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+    
+    /**
+     * Método que establece la descripción de una tarea.
+     * @param descripcion Cadena de texto que contiene la información de una tarea.
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    /**
+     * Método que devuelve el estado de una tarea.
+     * @return El estado de una tarea: no completada (falso) o completada (verdadero).
+     */
+    public boolean isCompletada() {
+        return this.completada;
+    }
+    /**
+     * Método que establece el estado de una tarea.
+     * @param completada 
+     */
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+    
+    
+    
+}
---
+++ src/gestortareasengrupo/main.java
@@ -0,0 +1,7 @@
+package gestortareasengrupo;
+public class main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
